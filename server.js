5205a0f089053bce9b81d8e28a1d6173095a6ac0
Hamzamt99 <tamarihamza4@gmail.com>
2023-05-20 11:55:34 +0000

    edit the array

--- server.js
+++ server.js
@@ -160,5 +160,6 @@ function Error500(err, req, res) {
 
 }
 client.connect().then( ()=> {
-    app.listen(PORT, () =>  console.log(`server run on ${PORT}`))
+
+    app.listen(PORT, () =>  console.log(`serever run on ${PORT}`))
 }).catch(error=> console.log(error))
